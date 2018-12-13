69c11a8896151dd6f355e41e07b14d26f1ce25f6
OguraDaiki <8hachibee125@gmail.com>
2018-12-13 15:57:20 +0900

    supoprt memoization and paging

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React, {Component, ReactNode} from 'react';
 
+import memoizeOne from 'memoize-one';
+
+function slicedItems<T>(items: ReadonlyArray<T>, page: number, contentsPerPage: number, previousPageBuffer: number): ReadonlyArray<T> {
+  const currentHead = page * contentsPerPage;
+  return items.slice(Math.max(0, currentHead - contentsPerPage * previousPageBuffer), currentHead + contentsPerPage);
+}
+
 const PREVIOUS_PAGE_BUFFER = 2;
 const CONTENT_PER_PAGE = 50;
 
@@ -8,8 +15,10 @@ interface State {
 }
 
 interface ChildArgument<T> {
+  page: number;
   renderPrevCreatives(): void;
   renderNextCreatives(): void;
+  renderPage(page: number): void;
   throttledItems: ReadonlyArray<T>;
 }
 
@@ -17,7 +26,7 @@ interface Props<T> {
   items: ReadonlyArray<T>;
   previousPageBuffer?: number;
   contentsPerPage?: number;
-  children(arg: ChildArgument<T>): ReactNode;
+  children(arg: Partial<ChildArgument<T>>): ReactNode;
 }
 
 export default class ListThrottle<T> extends Component<Props<T>, State> {
@@ -47,21 +56,17 @@ export default class ListThrottle<T> extends Component<Props<T>, State> {
     }
   };
 
-  // TODO: consider memoization
-  slicedItems = (): ReadonlyArray<T> => {
-    // cannot be undefined because of defaultProps but TypeScript never know about that
-    const contentsPerPage = this.props.contentsPerPage!;
-    const previousPageBuffer = this.props.previousPageBuffer!;
-
-    const currentHead = this.state.page * contentsPerPage;
-    return this.props.items.slice(Math.max(0, currentHead - contentsPerPage * previousPageBuffer), currentHead + contentsPerPage);
+  renderPage = (page: number) => {
+    this.setState({page});
   };
 
   render() {
     return this.props.children({
+      page: this.state.page,
       renderPrevCreatives: this.renderPrevCreatives,
       renderNextCreatives: this.renderNextCreatives,
-      throttledItems: this.slicedItems(),
+      renderPage: this.renderPage,
+      throttledItems: slicedItems(this.props.items, this.state.page, this.props.contentsPerPage!, this.props.previousPageBuffer!),
     });
   }
 }
